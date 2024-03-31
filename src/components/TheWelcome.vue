dbf1d00c60ab56b36fa4ad7e82d66001f2525f68
dora <dora924666990@gmail.com>
2024-04-01 00:34:29 +0800

    初始化

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,72 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Product Introduce</template>
+    <h6>
+Privacy is a basic right that everyone should be respected and protected. However, with the popularity of social media and digital communications,
+
+     Our personal privacy is increasingly vulnerable to invasion. Therefore, we need a method that can help us protect personal privacy
+
+     tool, and this tool is [Privacy Safe]. [Privacy Safe] is a special application that can
+
+     Lets you lock away sensitive or private content. By using [Privacy Vault], you can protect this content from any
+
+     Who snoops, steals or misuses it, even yourself. The biggest feature of [Privacy Safe] is that you can store the contents
+
+     Once the content is locked and set to a specific date and time, the content cannot be unlocked until then. this means,
+
+     If you have any sensitive content that needs to be properly protected, you can use [Privacy Vault] to protect it and set
+
+     Future date and time so that even if you lose your phone, your private data will not be lost. In summary, if you are insuring
+
+     If you attach great importance to protecting personal privacy, then [Privacy Safe] is an application you must not miss. You can
+
+     It splits private content and saves it in scattered ways to protect your privacy.
+     <p>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.0.apk">Dora Box v1.0</a>
+        MD5:7575550576575c3c35da5d082b0a00d7
+     </p>
+      <p>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.1.apk">Dora Box v1.1</a>
+        MD5:8bcdb082e43da631c79b12a6a202ae27
+     </p>
+     <p>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.2.apk">Dora Box v1.2</a>
+        MD5:92ad109b4348407de19b0a10e5e8b4f2
+     </p>
+     <p>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.3.apk">Dora Box v1.3</a>
+        MD5:97c996430b93dc58b501529e1ce64203
+     </p>
+
+     <p>
+        <a href="https://dorachat.oss-cn-hongkong.aliyuncs.com/Dora_Box_1.4.apk">Dora Box v1.4</a>
+        MD5:b3ab8eabc7e4d18da384e3cc2b4097da
+     </p>
+    </h6>
+    <br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Contact Us</template>
+     <p>
+    <a href="https://github.com/dora4">Github</a>
+     </p>
+
+  </WelcomeItem>
+</template>
